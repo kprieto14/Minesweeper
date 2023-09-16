3b2321fe68e7e89377d8963a5e8c59fbd5ed216f
Kristy Prieto <kprieto14@gmail.com>
2023-09-16 15:22:59 -0400

    Finish static implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,82 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div>
+      <header>
+        <h1>Minesweeper <button>New Game</button></h1>
+      </header>
+
+      <main className='easy'>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+      </main>
+
+      <footer>
+        <h2>Made with 🫶🏽 in Florida</h2>
+      </footer>
+    </div>
+  )
 }
