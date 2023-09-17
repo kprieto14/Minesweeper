680264c2c4019f1619ed8daca48d36f490e5f7f2
Kristy Prieto <kprieto14@gmail.com>
2023-09-17 18:27:38 -0400

    Adds and update state with API interactions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,144 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+type Square = '_' | 'F' | ' ' | '*' | '@' | number
+type Row = [Square, Square, Square, Square, Square, Square, Square, Square]
+type Board = [Row, Row, Row, Row, Row, Row, Row, Row]
+type Game = {
+  board: Board
+  id: null | number
+  difficulty: null | number
+  state: null | string
+  mines: null | number
+}
 
 export function App() {
+  const [game, setGame] = useState<Game>({
+    board: [
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' ']
+    ],
+    id: null,
+    difficulty: null,
+    state: null,
+    mines: null
+  })
+
+  async function handleClickCell(row: number, col: number) {
+    if (
+      // No game id
+      game.id === undefined ||
+      // A game is finished
+      game.state === 'won' ||
+      game.state === 'lost' ||
+      // The space isn't blank
+      game.board[row][col] !== ' '
+    ) {
+      return
+    }
+    
+    // Generate the URL we need
+    const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/check`
+    // Make an object to send as JSON
+    const body = { row, col }
+    // Make a POST request to make a move
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+    if (response.ok) {
+      // Get the response as JSON
+      const newGame = await response.json()
+      // Make that the new state!
+      setGame(newGame)
+    }
+  }
+
+  async function handleNewGame() {
+    // Make a POST request to ask for a new game
+    const response = await fetch(
+      'https://minesweeper-api.herokuapp.com/games',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+    if (response.ok) {
+      // Get the response as JSON
+      const newGame = await response.json() as Game
+      // Make that the new state!
+      setGame(newGame)
+    }
+  }
+
+  async function handleRightClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>, row: number, col: number) {
+    event.preventDefault()
+
+    if (
+      // No game id
+      game.id === undefined ||
+      // A game is finished
+      game.state === 'won' ||
+      game.state === 'lost' 
+    ) {
+      return
+    }
+    
+    // Generate the URL we need
+    const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/flag`
+    // Make an object to send as JSON
+    const body = { row, col }
+    // Make a POST request to make a move
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+    if (response.ok) {
+      // Get the response as JSON
+      const newGame = await response.json()
+      // Make that the new state!
+      setGame(newGame)
+    }
+  }
+
+  function newHeader() {
+    if(game.state === "new") {
+      return `New game started. Find ${game.mines} 💣`
+    } else if(game.state === "playing") {
+      return `Minesweeping in progress. ${game.mines} 💣 left`
+    } else if(game.state === 'won' || game.state === 'lost')
+      return `Game ${game.state}`
+  }
+
+  const header = game.state ? newHeader() : 'Minesweeper'
+
   return (
     <div>
       <header>
-        <h1>Minesweeper <button>New Game</button></h1>
+        <h1>{header} - <button onClick={handleNewGame}>New Game</button></h1>
       </header>
 
       <main className='easy'>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+        {game.board.map((boardRow, rowIndex) => {
+        return boardRow.map((cell, columnIndex) => {
+          return (
+            <button
+              key={columnIndex}
+              className={cell === ' ' || cell === 'F' ? undefined : 'reveal'}
+              onClick={() => handleClickCell(rowIndex, columnIndex)}
+              onContextMenu={(event) => handleRightClick(event, rowIndex, columnIndex)}>
+              {cell}
+            </button>
+            )
+          })
+        })}
       </main>
 
       <footer>
